8e3ac04802ece53d8703014c633f6bd09688e734
Desta Equar <154808732+Desta21m@users.noreply.github.com>
2024-06-17 00:39:41 +0300

    love

---
+++ project1-networking/cliate-side/src/Server.java
@@ -0,0 +1,17 @@
+import java.net.*;
+import java.io.*;
+public class Server {
+    public static void main(String[] arg) throws IOException{
+        ServerSocket ss= new ServerSocket(7777);
+        Socket s=ss.accept();
+
+        OutputStream out=s.getOutputStream();
+        PrintStream ps= new PrintStream(out);
+
+        ps.println("desta");
+        ps.println("hello h r u?");
+        ps.close();
+
+    }
+    
+}
---
+++ project1-networking/cliate-side/src/i_love.java
@@ -0,0 +1,173 @@
+class i_love { 
+    public static void main (String[] arg){
+        String sp= "                       ";
+        String sp1="                   ";
+        String sp2="                 ";
+        String sp3="                ";
+        String sp4="               ";
+        String sp5="               ";
+        String p5="         ";
+        String p4="             ";
+        String p3="               ";
+        String p2="                 ";
+        String p1="                 ";
+        String v1= "         ";
+        String v2="      ";
+        String v3="     ";
+        String v4="     ";
+        String v5="     ";
+        String v6="      ";
+        String v7="       ";
+        String v8="         ";
+        String v9="           ";
+        String v10="              ";
+        String vv1= "                   ";
+        String vv2="                 ";
+        String vv3="               ";
+        String vv4="             ";
+        String vv5="           ";
+        String vv6="         ";
+        String vv7="       ";
+        String vv8="     ";
+        String vv9="   ";
+        String vv10=" ";
+        String u1 ="          ";
+        String u2 ="                    ";
+        String u3 ="                    ";
+        String u4 ="                    ";
+        String u5 ="                    ";
+        String u6 ="          ";
+        String u7 ="                    ";
+        String u8 ="                     ";
+        String u9 ="                       ";
+        String u10="                          ";
+        String uu1 ="                      ";
+        String uu2 ="                      ";
+        String uu3 ="                      ";
+        String uu4 ="                      ";
+        String uu5 ="                      ";
+        String uu6 ="                      ";
+        String uu7 ="                      ";
+        String uu8 ="                    ";
+        String uu9 ="                 ";
+        String uu10="          ";
+
+
+
+        System.out.println();
+        System.out.println();
+
+        System.out.print("*********************     ");
+        System.out.println();
+        // System.out.print("*");
+
+        for(int i=0; i<13;i++){
+            System.out.print("          *");
+            System.out.print("                         *");
+            if(i==3){
+                System.out.print(sp+"*");
+                System.out.print(" *");
+                System.out.print(v1+" *");
+                System.out.print(vv1+"*");
+                System.out.print("  ***********");
+                System.out.print(u1+"*");
+                System.out.print(uu1+"*");
+            }
+            if(i==4){
+                System.out.print(sp1+"*");
+                System.out.print(p5+"*");
+                System.out.print(v2+" *");
+                System.out.print(vv2+"*");
+                System.out.print("   *");
+                System.out.print(u2+"*");
+                System.out.print(uu2+"*");
+            }
+            if(i==5){
+                System.out.print(sp2+"*");
+                System.out.print(p4+"*");
+                System.out.print(v3+" *");
+                System.out.print(vv3+"*");
+                System.out.print("    *");
+                System.out.print(u3+"*");
+                System.out.print(uu3+"*");
+            }
+            if(i==6){
+                System.out.print(sp3+"*");
+                System.out.print(p3+"*");
+                System.out.print(v4+" *");
+                System.out.print(vv4+"*");
+                System.out.print("     *");
+                System.out.print(u4+"*");
+                System.out.print(uu4+"*");
+            }
+            if(i==7){
+                System.out.print(sp4+"*");
+                System.out.print(p2+"*");
+                System.out.print(v5+" *");
+                System.out.print(vv5+"*");
+                System.out.print("      *");
+                System.out.print(u5+"*");
+                System.out.print(uu5+"*");
+            }
+            if(i==8){
+                System.out.print(sp5+"*");
+                System.out.print(p1+"*");
+                System.out.print(v6+" *");
+                System.out.print(vv6+"*");
+                System.out.print("       ***********");
+                System.out.print(u6+"*");
+                System.out.print(uu6+"*");
+            }
+            if(i==9){
+                // System.out.print(p3+"l4*");
+                System.out.print(sp4+"*");
+                System.out.print(p2+"*");
+                System.out.print(v7+" *");
+                System.out.print(vv7+"*");
+                System.out.print("        *");
+                System.out.print(u7+"*");
+                System.out.print(uu7+"*");
+              
+            }
+            if(i==10){
+                // System.out.print(p3+"l3*");
+                System.out.print(sp3+"*"); 
+                System.out.print(p3+"*"); 
+                System.out.print(v8+" *");  
+                System.out.print(vv8+"*");
+                System.out.print("         *"); 
+                System.out.print(u8+"*");
+                System.out.print(uu8+"*");
+            }
+            if(i==11){
+                // System.out.print(p2+"l2*");
+                System.out.print(sp2+"*");
+                System.out.print(p4+"*");
+                System.out.print(v9+" *");
+                System.out.print(vv9+"*");
+                System.out.print("          *");
+                System.out.print(u9+"*");
+                System.out.print(uu9+"*");
+            }
+            if(i==12){
+                // System.out.print(p1+"l1*");
+                System.out.print(sp1+"*");
+                System.out.print(p5+"*");
+                System.out.print(v10+" *");
+                System.out.print(vv10+"*");
+                System.out.print("           *");
+                System.out.print(u10+"*");
+                System.out.print(uu10+"*");
+            }
+            System.out.println();
+        }
+        System.out.print("*********************");
+        System.out.print("               ***************");
+        System.out.print("        * * *");
+        System.out.print("                   *");
+        System.out.print("            ***********");
+        System.out.print("                   *  *");
+
+
+    }
+}
