6cd52bfdee809f2f521e9731117b7aa6504dd58a
Desta Equar <154808732+Desta21m@users.noreply.github.com>
2024-06-17 23:20:56 +0300

    ew

--- project1-networking/server-side/src/advans/bestServer.java
+++ project1-networking/server-side/src/advans/bestServer.java
@@ -5,41 +5,41 @@ import java.io.*;
 class bestServer {
   public static void main(String[] arg)throws Exception{
 
-    ServerSocket ss=new ServerSocket(8877);
-    System.out.println("the server is waiteing to connect to cliate......");
+      ServerSocket ss=new ServerSocket(8877);
+      System.out.println("the server is waiteing to connect to cliate......");
 
-    Socket s=ss.accept();
-    System.out.println("the cliate connected succesfully");
+      Socket s = ss.accept();
+      System.out.println("the cliate connected succesfully");
 
-    InputStream is=s.getInputStream();
-    BufferedReader cliateReader= new BufferedReader(new InputStreamReader(is));
+      InputStream is = s.getInputStream();
+      BufferedReader cliateReader = new BufferedReader(new InputStreamReader(is));
 
-    BufferedReader keybordReader= new BufferedReader(new InputStreamReader(System.in));
+      BufferedReader keybordReader= new BufferedReader(new InputStreamReader(System.in));
 
-    OutputStream os=s.getOutputStream();
-    PrintStream cliateWriter =new PrintStream(os);
+      OutputStream os=s.getOutputStream();
+      PrintStream cliateWriter =new PrintStream(os);
 
-    String cliateMessage;
-    String ServerMessage;
+      String cliateMessage;
+      String ServerMessage;
 
-    while(true){
-        cliateMessage=cliateReader.readLine();
-        if(cliateMessage.equals("bye" )|| cliateMessage==null){
-            System.out.println("coonnection is finfish");
-            break;
-        }
-        System.out.println("cliate: "+cliateMessage);
-        System.out.print("server: ");
-        ServerMessage=keybordReader.readLine();
-        cliateWriter.println(ServerMessage);
+      while(true){
+          cliateMessage=cliateReader.readLine();
+          if(cliateMessage.equals("bye" )|| cliateMessage==null){
+              System.out.println("coonnection is finfish");
+              break;
+          }
+          System.out.println("cliate: "+cliateMessage);
+          System.out.print("server: ");
+          ServerMessage=keybordReader.readLine();
+          cliateWriter.println(ServerMessage);
 
 
+      }
+      cliateReader.close();
+      keybordReader.close();
+      cliateWriter.close();
+      s.close();
+      ss.close();
     }
-    cliateReader.close();
-    keybordReader.close();
-    cliateWriter.close();
-    s.close();
-    ss.close();
-  }
 }
     
